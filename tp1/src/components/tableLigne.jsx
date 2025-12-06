953b137bdbc16902ba44482903aa6a84119b201e
Mohamed yassine chliah <mohamdysn2365@gmail.com>
2025-12-06 12:37:42 +0100

    ajout des components de table et de ligne

---
+++ tp1/src/components/tableLigne.jsx
@@ -0,0 +1,15 @@
+function TableTr () {
+    return (
+        <tr>
+            <td>001</td>
+            <td>nom porduit</td>
+            <td>image</td>
+            <td>1.5</td>
+            <td>50</td>
+            <td>
+                <button className="btnSupp">supprimer</button>
+            </td>
+        </tr>
+    )
+}
+export default TableTr;
